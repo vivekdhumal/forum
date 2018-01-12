3582b7c57e24a611dee6892131a20ba4c34818b9
Vivek Dhumal <vivekdhumalmail@gmail.com>
2018-01-12 20:51:35 +0530

    Query refactor 2

--- app/Reply.php
+++ app/Reply.php
@@ -3,11 +3,16 @@
 namespace App;
 
 use App\Favorite;
+use App\Traits\Favoritable;
 use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
+    use Favoritable;
+
     protected $guarded = [];
+
+    protected $with = ['owner', 'favorites'];
 
     /**
      * Get the owner associate with the reply.
@@ -17,24 +22,5 @@ class Reply extends Model
     public function owner()
     {
         return $this->belongsTo(User::class, 'user_id');
-    }
-
-    public function favorites()
-    {
-        return $this->morphMany(Favorite::class, 'favorited');
-    }
-
-    public function favorite()
-    {
-        $attributes = ['user_id' => auth()->id()];
-
-        if(!$this->favorites()->where($attributes)->exists()) {
-            $this->favorites()->create($attributes);
-        }
-    }
-
-    public function isFavorited()
-    {
-        return $this->favorites()->where('user_id', auth()->id())->exists();
     }
 }
--- app/Thread.php
+++ app/Thread.php
@@ -9,6 +9,8 @@ class Thread extends Model
 {
     protected $guarded = [];
 
+    protected $with = ['creator', 'channel'];
+
     protected static function boot()
     {
         parent::boot();
@@ -35,9 +37,7 @@ class Thread extends Model
      */
     public function replies()
     {
-        return $this->hasMany(Reply::class)
-            ->withCount('favorites')
-            ->with('owner');
+        return $this->hasMany(Reply::class);
     }
 
     /**

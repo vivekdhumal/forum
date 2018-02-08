c0d3c049fbf87b2dad5e2a2be47de5ca84ed9b5e
Vivek Dhumal <vivekdhumalmail@gmail.com>
2018-02-08 07:08:45 +0530

    Code refactored

--- resources/assets/js/components/Reply.vue
+++ resources/assets/js/components/Reply.vue
@@ -16,12 +16,14 @@
 
         <div class="panel-body">
             <div v-if="editing">
-                <div class="form-group">
-                    <textarea class="form-control" v-model="body"></textarea>
-                </div>
+                <form @submit.prevent="update">
+                    <div class="form-group">
+                        <textarea class="form-control" v-model="body" required></textarea>
+                    </div>
 
-                <button type="submit" class="btn btn-xs btn-primary" @click="update">Update</button>
-                <button type="button" class="btn btn-xs btn-link" @click="editing = false">Cancel</button>
+                    <button type="submit" class="btn btn-xs btn-primary">Update</button>
+                    <button type="button" class="btn btn-xs btn-link" @click="editing = false">Cancel</button>
+                </form>
             </div>
 
             <div v-else v-text="body"></div>
